3db6f2ad4f933290804efbce666cdf0dcec70f76
HackerKid42 <87086842+CoolestGuy94@users.noreply.github.com>
2023-06-19 23:59:10 +0300

    pretyt col

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 //you need jsmacros to run this script, then set Events to Bossbar
-Chat.say(Array.from(World.getBossBars().values())[0].getName().getString())
+const bossBars = World.getBossBars();
+if (Object.keys(bossBars).length > 0) {
+  const firstBossBar = Array.from(bossBars.values())[0];
+  if (firstBossBar && firstBossBar.getName && firstBossBar.getName().getString()) {
+    Chat.say(`${firstBossBar.getName().getString()}`);
+  }
+} else {
+  // do nothing
+}
+// rewrote for errorless experience. pretty cool!
+// it also may repeat the name of the bossbar, idk how to fix that yet.
